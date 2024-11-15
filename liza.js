3339c0240550b767eb8b652108c67a7054ebc8fd
Liza-byte-creator <Lizaxhaferi05@gmail.com>
2024-11-15 11:07:08 +0200

    Update liza.js

--- liza.js
+++ liza.js
@@ -5,3 +5,14 @@ var z=7;
 var k=8;
 var sum=x+y+z+k;
 console.log(sum);
+
+
+let x=5;
+let y=2;
+let l=10;
+let z=8;
+let k=9;
+
+let mesatarja(x+y+l+z+k)/5;
+
+document.write("Mesatarja e numrave eshte"+mesatarja);
